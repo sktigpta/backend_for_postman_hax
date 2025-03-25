e8a54502128f58520de836f925947b3ce0254ef5
Shaktidhar Gupta <sktigpta@gmail.com>
2025-03-26 03:42:51 +0530

    fg

--- index.js
+++ index.js
@@ -1,43 +1,10 @@
-require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-
-require("./config/firebase.js");
-
-const youtubeRoutes = require("./routes/youtubeRoutes.js");
-const searchQueriesRoute = require("./routes/searchQueries.js");
-const gettingPermissionIds = require("./routes/permissionRoutes.js");
-const processedRoutes = require("./routes/processedRoutes.js");
-const dmcaRoutes = require("./routes/dmcaRoutes.js");
-
 const app = express();
-
-const allowedOrigins = [
-  `http://localhost:${process.env.FRONTEND_PORT}`,
-  process.env.SECONDARY_FRONTEND_URL,
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: "GET,POST,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-    credentials: true,
-  })
-);
 
 app.use(express.json());
 
-app.use("/api/youtube", youtubeRoutes);
-app.use("/api/search-queries", searchQueriesRoute);
-app.use("/api/permissions", gettingPermissionIds);
-app.use("/api/processed", processedRoutes);
-app.use("/api/dmca", dmcaRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 module.exports = app;
